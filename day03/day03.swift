9e88f00708d387d073e670973ae4e20a9ad664ab
Zach <zachvaters@gmail.com>
2022-12-03 13:19:40 -0330

    Clean up away with the priority map

--- day03/day03.swift
+++ day03/day03.swift
@@ -2,60 +2,16 @@ import Foundation
 
 let lines = try String(contentsOfFile: "input.txt", encoding: .utf8).split(separator: "\n")
 
-let priorityMap: [String: Int] = [
-  "a": 1,
-  "b": 2,
-  "c": 3,
-  "d": 4,
-  "e": 5,
-  "f": 6,
-  "g": 7,
-  "h": 8,
-  "i": 9,
-  "j": 10,
-  "k": 11,
-  "l": 12,
-  "m": 13,
-  "n": 14,
-  "o": 15,
-  "p": 16,
-  "q": 17,
-  "r": 18,
-  "s": 19,
-  "t": 20,
-  "u": 21,
-  "v": 22,
-  "w": 23,
-  "x": 24,
-  "y": 25,
-  "z": 26,
-  "A": 27,
-  "B": 28,
-  "C": 29,
-  "D": 30,
-  "E": 31,
-  "F": 32,
-  "G": 33,
-  "H": 34,
-  "I": 35,
-  "J": 36,
-  "K": 37,
-  "L": 38,
-  "M": 39,
-  "N": 40,
-  "O": 41,
-  "P": 42,
-  "Q": 43,
-  "R": 44,
-  "S": 45,
-  "T": 46,
-  "U": 47,
-  "V": 48,
-  "W": 49,
-  "X": 50,
-  "Y": 51,
-  "Z": 52,
-]
+func rucksackPriority(_ char: Character) -> Int {
+  let ascii = Int(String(char).unicodeScalars.first!.value)
+  if ascii >= 97 && ascii <= 122 {
+    return ascii - 96
+  } else if ascii >= 65 && ascii <= 90 {
+    return ascii - 38
+  } else {
+    return 0
+  }
+}
 
 func partOne() -> Int {
   var sum = 0
@@ -65,7 +21,7 @@ func partOne() -> Int {
     var appeared = Set<String>()
     for char in firstHalf {
       if secondHalf.contains(char) && !appeared.contains(String(char)) {
-        sum += priorityMap[String(char)]!
+        sum += rucksackPriority(char)
         appeared.insert(String(char))
       }
     }
@@ -82,7 +38,7 @@ func partTwo() -> Int {
     var appeared = Set<String>()
     for char in firstLine {
       if secondLine.contains(char) && thirdLine.contains(char) && !appeared.contains(String(char)) {
-        sum += priorityMap[String(char)]!
+        sum += rucksackPriority(char)
         appeared.insert(String(char))
       }
     }
